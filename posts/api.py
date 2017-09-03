3fcde395c7b3458246d6dccab47c67f934633467
EthanVG <ethanvascellaro@gmail.com>
2017-09-03 19:34:49 +0000

    Initial commit

---
+++ posts/api.py
@@ -0,0 +1,64 @@
+import json
+
+from flask import request, Response, url_for
+from jsonschema import validate, ValidationError
+
+from . import models
+from . import decorators
+from posts import app
+from .database import session
+    
+@app.route("/api/posts", methods=["GET"])
+@decorators.accept("application/json")
+def posts_get():
+    """ Get a list of posts """
+    
+    title_like = request.args.get("title_like")
+    body_like = request.args.get("body_like")
+    
+    posts = session.query(models.Post)
+    
+    if title_like:
+        posts = posts.filter(models.Post.title.contains(title_like))
+    if body_like:
+        posts = posts.filter(models.Post.body.contains(body_like))
+        
+    posts = posts.order_by(models.Post.id)
+
+    data = json.dumps([post.as_dictionary() for post in posts])
+    return Response(data, 200, mimetype="application/json")
+    
+@app.route("/api/posts/<int:id>", methods=["GET"])
+@decorators.accept("application/json")
+def post_get(id):
+    """ Single post endpoint """
+    post = session.query(models.Post).get(id)
+    
+    if not post:
+        message = "Could not find post with id {}".format(id)
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+
+    data = json.dumps(post.as_dictionary())
+    return Response(data, 200, mimetype="application/json")
+    
+@app.route("/api/posts/<int:id>", methods=["DELETE"])
+@decorators.accept("application/json")
+def post_delete(id):
+    """ Delete post endpoint """
+    post = session.query(models.Post).get(id)
+    
+    if not post:
+        message = "Could not find post with id {}".format(id)
+        data = json.dumps({"message": message})
+        return Response(data, 404, mimetype="application/json")
+    
+    session.delete(post)
+    session.commit()
+    
+    posts = session.query(models.Post).order_by(models.Post.id)
+
+    data = json.dumps([post.as_dictionary() for post in posts])
+    return Response(data, 200, mimetype="application/json")
+    
+    
